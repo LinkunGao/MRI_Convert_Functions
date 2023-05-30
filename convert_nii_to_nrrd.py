120c843176d41842005f76512012718c8155284c
skycoco <skycoco1994@gmail.com>
2023-05-30 14:53:31 +1200

    update convert nii to nrrd function

--- convert_nii_to_nrrd.py
+++ convert_nii_to_nrrd.py
@@ -1,12 +1,18 @@
 import SimpleITK as sitk
-from pathlib import Path
+
 
 def convert_nii_to_nrrd():
-    source = Path(r'./path/to/mask.nii.gz')
-    dest = Path(r'./path/to/mask.nrrd')
+    source = [r'./import/img_inv_contrast_0-1.nii.gz', r'./import/img_inv_contrast_0-2.nii.gz',
+              r'./import/img_inv_contrast_0-3.nii.gz', r'./import/img_inv_contrast_0-4.nii.gz']
+    dest = [r'./export/r1.nrrd',r'./export/r2.nrrd',r'./export/r3.nrrd',r'./export/r4.nrrd']
 
-    input_image = sitk.ReadImage(source)
-    sitk.WriteImage(input_image, dest)
+    pre = r'H:\docker\import_nrrd\26\new_0.nrrd'
+    pre_image = sitk.ReadImage(pre)
+    for i in range(len(source)):
+
+        input_image = sitk.ReadImage(source[i])
+        input_image.CopyInformation(pre_image)
+        sitk.WriteImage(input_image, dest[i])
 
 
 if __name__ == "__main__":
