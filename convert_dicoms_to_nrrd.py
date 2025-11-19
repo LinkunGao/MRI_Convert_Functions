62b1180a05c90592561829d2cca33202a54b7a69
LinkunGao <gaolinkun123@gmail.com>
2025-11-20 12:11:54 +1300

    chore: update convert dcms to nrrd function

--- convert_dicoms_to_nrrd.py
+++ convert_dicoms_to_nrrd.py
@@ -1,4 +1,5 @@
 import SimpleITK as sitk
+import os
 
 '''
 A python script for convert dicom files to nrrd file with sepreating contrast.
@@ -13,36 +14,73 @@ you will get 5 nrrd files with different contrast!
 
 '''
 
-def dcmseries2nrrd(filepath):
-    datapath = filepath
-    allFilesPath = []
-    max = 0
-    contrstIdx = 0
 
-    dcms_name = sitk.ImageSeriesReader.GetGDCMSeriesFileNames(datapath)
+def dcm_series_to_nrrd(dcm_folder_path, output_dir):
+    # Assign DCM paths with different contrasts to separate lists.
+    all_files_path = _divide_dcms_to_different_contrasts(dcm_folder_path)
+    # Convert each contrast dcms to a nrrd file.
+    _convert_dicoms_contrast_to_nrrd(all_files_path, output_dir)
+
+
+def _divide_dcms_to_different_contrasts(dcm_folder_path):
+    """
+
+    :param dcm_folder_path: dicoms folder path, may include multiple contrast dicom files
+    :return: each contrast dicom file is divided into different lists
+    """
+    all_files_path = []
+    max_instance_num = 0
+
+    dcms_name = sitk.ImageSeriesReader.GetGDCMSeriesFileNames(dcm_folder_path)
 
     for dcm in dcms_name:
         dicom = sitk.ReadImage(dcm)
-        instanceNum = int(dicom.GetMetaData('0020|0012'))
-        if max < instanceNum:
-            max= instanceNum
+        instance_num = int(dicom.GetMetaData('0020|0012'))
+        if max_instance_num < instance_num:
+            max_instance_num = instance_num
 
-    for i in range(max):
-        allFilesPath.append([])
+    for i in range(max_instance_num):
+        all_files_path.append([])
 
     for dcm in dcms_name:
         dicom = sitk.ReadImage(dcm)
-        instanceNum = int(dicom.GetMetaData('0020|0012'))-1
-        allFilesPath[instanceNum].append(dcm)
+        instance_num = int(dicom.GetMetaData('0020|0012')) - 1
+        all_files_path[instance_num].append(dcm)
 
-    dcms_read = sitk.ImageSeriesReader()
+    return all_files_path
 
-    for list in allFilesPath:
-        dcms_read.SetFileNames(list)
-        dcms_series = dcms_read.Execute()
-        name = "new_" + str(contrstIdx)
-        contrstIdx+= 1
-        sitk.WriteImage(dcms_series, name + '.nrrd')
 
-filepath = "/Your/dicom/files/path"
-dcmseries2nrrd(filepath)
+def _convert_dicoms_contrast_to_nrrd(all_files_path, output_dir):
+    """
+
+    :param all_files_path: all contrast dicom files are divided into different lists
+    :param output_dir: save nrrd files path
+    :return:
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    contrast_idx = 0
+    dcm_reader = sitk.ImageSeriesReader()
+
+    for dcm_files_path in all_files_path:
+        name = "new_" + str(contrast_idx)
+        output_path = os.path.join(output_dir, f'{name}.nrrd')
+        contrast_idx += 1
+
+        _convert(dcm_reader, dcm_files_path, output_path)
+
+
+def _convert(dcm_reader, dcm_files_path, output_path):
+    """
+    :param dcm_reader: SimpleITK Image Reader
+    :param dcm_files_path: one contrast dicom files
+    :param output_path: nrrd file path
+    :return:
+    """
+    dcm_reader.SetFileNames(dcm_files_path)
+    dcm_series = dcm_reader.Execute()
+    sitk.WriteImage(dcm_series, output_path)
+
+
+dcm_folder_path = "/Your/dicom/files/path"
+output_dir = "./test"
+dcm_series_to_nrrd(dcm_folder_path, output_dir)
